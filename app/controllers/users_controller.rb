d8d0c7504e45ee3d24b08161e4f59d4974d45c63
jungtaejtkim <jungtae.jt.kim@gmail.com>
2016-11-29 16:42:43 +0900

    add user and stock association and display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 	def my_portfolio
-
+		@user_stocks = current_user.stocks # 이미 many to many asso 가 있으니 이런식으로 instance var 로 설정 가능 
+		@user = current_user
 
 	end
 	
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,27 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+	has_many :user_stocks
+	has_many :stocks, through: :user_stocks
+
+	def can_add_stock?(ticker_symbol)
+		under_stock_limit? && !stock_already_added?(ticker_symbol)
+
+	end
+
+	def under_stock_limit?
+		(user_stocks.count<10)
+
+	end
+
+	def stock_already_added?(ticker_symbol)  # 기존 db 가 unique 하게 구성된게 아니니까 db 를 살펴보게나
+		stock=Stock.find_by_ticker(ticker_symbol)
+		return false unless stock
+		user_stocks.where(stock_id: stock.id).exists?
+	end
+
+
 end
